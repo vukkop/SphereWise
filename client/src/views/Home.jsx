774772e91d71d1a6df3b0e9c419bc0c71a45daef
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-01-11 15:24:22 -0800

    Home page rows

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,17 +2,42 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div className="card w-full  bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="text-left text-4xl mt-5">Tailored Solutions for Every Business Size</h2>
-        <p className="text-left mt-5">At SphereWise Tech, we champion the growth of businesses, regardless of size. Our bespoke website and software solutions aren't confined by scale; they're crafted to elevate small businesses to new heights. We believe that no endeavor is too modest. From startups to local enterprises, our commitment remains steadfast in delivering intuitive websites and custom software that catalyze growth. Our focus? Empowering every business, ensuring they harness the power of technology to expand their horizons and redefine success. Let's embark on this journey together—transforming small ventures into impactful enterprises, one innovative solution at a time.
-        </p>
+    <div>
 
-
-
-        <div className="card-actions justify-center mt-3 mb-3">
-          <button className="btn btn-outline btn-info">Find Out More</button>
+      <div className="card w-full  bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-left text-4xl mt-5">Tailored Solutions for Every Business Size</h2>
+          <p className="text-left mt-5">At SphereWise Tech, we champion the growth of businesses, regardless of size. Our bespoke website and software solutions aren't confined by scale; they're crafted to elevate small businesses to new heights. We believe that no endeavor is too modest. From startups to local enterprises, our commitment remains steadfast in delivering intuitive websites and custom software that catalyze growth. Our focus? Empowering every business, ensuring they harness the power of technology to expand their horizons and redefine success. Let's embark on this journey together—transforming small ventures into impactful enterprises, one innovative solution at a time.
+          </p>
+          <div className="card-actions justify-center mt-3 mb-3">
+            <button className="btn btn-outline btn-info">Find Out More</button>
+          </div>
         </div>
+      </div>
+
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">What We Do</h3>
+      </div>
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">How We Work</h3>
+      </div>
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">Choosing Us</h3>
+      </div>
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">Our Team</h3>
+      </div>
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">Our Clients</h3>
+      </div>
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">Technologies</h3>
+      </div>
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">Reviews</h3>
+      </div>
+      <div className="row mt-3">
+        <h3 className="text-left text-4xl mt-5">Frequently Asked Questions</h3>
       </div>
     </div>
   )
