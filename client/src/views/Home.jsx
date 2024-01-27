a679816c62e8449a1099044e6f478f54997aab8f
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-01-27 15:30:47 -0800

    Creating hero

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Idea from '../assets/images/idea.jpg'
 import MRLogo from '../assets/ourCients/M&RSolutions.png'
 import PPLogo from '../assets/ourCients/PuppyPoodle.svg'
 
@@ -6,12 +7,17 @@ export const Home = () => {
   return (
     <>
       <div className='container mx-auto'>
-        <div className="my-10">
-          <h2 className="text-left text-4xl mt-5">Tailored Solutions for Every Business Size</h2>
-          <p className="text-left mt-5">At SphereWise Tech, we champion the growth of businesses, regardless of size. Our bespoke website and software solutions aren't confined by scale; they're crafted to elevate small businesses to new heights. We believe that no endeavor is too modest. From startups to local enterprises, our commitment remains steadfast in delivering intuitive websites and custom software that catalyze growth. Our focus? Empowering every business, ensuring they harness the power of technology to expand their horizons and redefine success. Let's embark on this journey together—transforming small ventures into impactful enterprises, one innovative solution at a time.
-          </p>
-          <div className="card-actions justify-center mt-3 mb-3">
-            <button className="btn btn-outline btn-info">Find Out More</button>
+        <div className="hero min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img src={Idea} className="max-w-sm rounded-lg shadow-2xl filter grayscale contrast-200 bg-gradient-to-r from-inherit to-transparent" />
+            <div>
+              <h2 className="text-left text-4xl mt-5">Tailored Solutions for Every Business Size</h2>
+              <p className="text-left mt-5">At SphereWise Tech, we champion the growth of businesses, regardless of size. Our bespoke website and software solutions aren't confined by scale; they're crafted to elevate small businesses to new heights. We believe that no endeavor is too modest. From startups to local enterprises, our commitment remains steadfast in delivering intuitive websites and custom software that catalyze growth. Our focus? Empowering every business, ensuring they harness the power of technology to expand their horizons and redefine success. Let's embark on this journey together—transforming small ventures into impactful enterprises, one innovative solution at a time.
+              </p>
+              <div className="justify-center mt-3 mb-3">
+                <button className="btn btn-outline btn-info">Find Out More</button>
+              </div>
+            </div>
           </div>
         </div>
 
