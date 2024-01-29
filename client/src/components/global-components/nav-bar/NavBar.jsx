47bf8b5e3f70fd73f78be731a7e3417e47171569
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-01-29 15:07:56 -0800

    Fixed NavBar Logo

--- client/src/components/global-components/nav-bar/NavBar.jsx
+++ client/src/components/global-components/nav-bar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../../../assets/logo/Side_Logo_full_dark_background.png'
+import Logo from '../../../assets/logo/Side_Logo.png'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
