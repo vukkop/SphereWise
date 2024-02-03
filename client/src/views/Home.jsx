568ae21a715635a398107df1f441b51aa9b07be3
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-02-03 15:13:30 -0800

    using span instead of icons

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -39,42 +39,42 @@ export const Home = () => {
           <div className="grid grid-cols-2 gap-16 text-left">
             <div>
               <div className='rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 via-blue-600 to-transparent '>
-                <i className="fa-solid fa-1"></i>
+                <span className='font-bold text-xl'>1</span>
               </div>
               <h4 className='text-xl font-semibold mb-1'>Certified experts</h4>
               <p>Certified business and tech development experts who ensure a business-focused approach.</p>
             </div>
             <div>
               <div className='rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 via-blue-600 to-transparent '>
-                <i className="fa-solid fa-2"></i>
+                <span className='font-bold text-xl'>2</span>
               </div>
               <h4 className='text-xl font-semibold mb-1'>Quick implementation</h4>
               <p>Velocity doubled by quality is what our Agile approach boils down to.</p>
             </div>
             <div>
               <div className='rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 via-blue-600 to-transparent '>
-                <i className="fa-solid fa-3"></i>
+                <span className='font-bold text-xl'>3</span>
               </div>
               <h4 className='text-xl font-semibold mb-1'>Technology Agnostic</h4>
               <p>Providing a tech stack wide and expansive enough to erase any limits of flexibility during each and every project.</p>
             </div>
             <div>
               <div className='rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 via-blue-600 to-transparent '>
-                <i className="fa-solid fa-4"></i>
+                <span className='font-bold text-xl'>4</span>
               </div>
               <h4 className='text-xl font-semibold mb-1'>Flexibility</h4>
               <p>We can integrate your product with any required platform so that it moves your business forward.</p>
             </div>
             <div>
               <div className='rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 via-blue-600 to-transparent '>
-                <i className="fa-solid fa-5"></i>
+                <span className='font-bold text-xl'>5</span>
               </div>
               <h4 className='text-xl font-semibold mb-1'>Compliance</h4>
               <p>Regardless of the industry, we build software that does not react to, but prevents cyber security and legal issues.</p>
             </div>
             <div>
               <div className='rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 via-blue-600 to-transparent '>
-                <i className="fa-solid fa-6"></i>
+                <span className='font-bold text-xl'>6</span>
               </div>
               <h4 className='text-xl font-semibold mb-1'>Adaptability</h4>
               <p>Avenga builds products with codes, adapting to the client's requirements, not vice versa.</p>
