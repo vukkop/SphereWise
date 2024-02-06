c9f7fdb61f387c9f0a905a5b8bf4bd541c607a4b
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-02-06 13:38:54 -0800

    Fixed NavBar for light mode

--- client/src/components/global-components/nav-bar/NavBar.jsx
+++ client/src/components/global-components/nav-bar/NavBar.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import Logo from '../../../assets/logo/Side_Logo.png'
 import { Link } from 'react-router-dom'
 
-
 const NavBar = ({ isLight, onUpdate }) => {
-
   const handleThemeChange = () => {
     onUpdate();
   }
 
   return (
-    <div className='container mx-auto'>
-      <div className="navbar bg-base-100">
+    <div className={`navbar bg-base-100 ${isLight && 'bg-slate-800 text-slate-300'}`}>
+      <div className='container mx-auto'>
         <div className="navbar-start">
           <Link to={"/"}>
             <img src={Logo} alt="SphereWise Technology Logo" className='max-h-[55px]' />
@@ -19,14 +17,14 @@ const NavBar = ({ isLight, onUpdate }) => {
         </div>
 
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><Link to={"/"}>Home</Link></li>
-            <li><Link to={"/services"}>Services</Link></li>
-            <li><Link to={"/about"}>About</Link></li>
-            <li><Link to={"/contact"}>Contact</Link></li>
+          <ul className="menu menu-horizontal px-1 ">
+            <li ><Link className='focus:!text-slate-500' to={"/"}>Home</Link></li>
+            <li><Link className='focus:!text-slate-500' to={"/services"}>Services</Link></li>
+            <li><Link className='focus:!text-slate-500' to={"/about"}>About</Link></li>
+            <li><Link className='focus:!text-slate-500' to={"/contact"}>Contact</Link></li>
           </ul>
         </div>
-        <div className="navbar-end">
+        <div className="navbar-end flex items-center">
           <Link to={"/contact"} className="btn btn-info btn-sm md:btn-md lg:mr-3"><i className="fa-solid fa-phone"></i><span className='hidden md:flex'>Contact Us</span></Link>
 
           <label className="swap swap-rotate ml-4">
@@ -44,7 +42,7 @@ const NavBar = ({ isLight, onUpdate }) => {
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content items-end mt-1 right-0 z-[1] p-3 gap-2 shadow bg-base-100 rounded-box ">
+            <ul tabIndex={0} className={`menu menu-sm dropdown-content items-end mt-1 right-0 z-[1] p-3 gap-2 shadow bg-base-100 rounded-box ${isLight && 'text-slate-900'}`}>
               <li><Link to={"/"}>Home</Link></li>
               <li><Link to={"/services"}>Services</Link></li>
               <li><Link to={"/about"}>About</Link></li>
