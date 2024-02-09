0336fee35f9492b7229f88b34b236aa7d3c7b657
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-02-08 16:38:24 -0800

    Reviews implemented

--- client/src/components/home-components/Reviews.jsx
+++ client/src/components/home-components/Reviews.jsx
@@ -1,48 +1,83 @@
 import React from 'react'
 
 const Reviews = () => {
+
+  const reviews = [
+    {
+      review: 'Review 1.',
+      name: 'Name 1',
+      raiting: 10,
+    },
+    {
+      review: 'Review 2.',
+      name: 'Name 2',
+      raiting: 10,
+    },
+    {
+      review: 'Review 3.',
+      name: 'Name 3',
+      raiting: 10,
+    },
+    {
+      review: 'Review 4.',
+      name: 'Name 4',
+      raiting: 10,
+    },
+  ]
+
   return (
     <div className="row my-10">
-      <h3 className="text-left text-4xl mt-5">Reviews</h3>
+      <h3 className="text-left text-4xl my-7">Reviews</h3>
 
 
-      <div className="carousel w-full my-5">
-        <div id="slide1" className="carousel-item relative w-full">
-          <div className='flex justify-center items-center w-full'>
-            <p> REVIEW</p>
-          </div>
+      <div className="carousel w-full">
 
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">❮</a>
-            <a href="#slide2" className="btn btn-circle">❯</a>
+        {reviews.map((reviews, i) => (
+          <div key={i} id={`slide${i}`} className="carousel-item relative w-full">
+            <div className='flex justify-center items-center w-full'>
+              <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body text-center">
+                  <blockquote>
+                    <p class="text-2xl font-semibold text-gray-900 dark:text-white">"{reviews.review}"</p>
+                  </blockquote>
+                  <figcaption class="flex justify-center items-center mt-6 space-x-3 rtl:space-x-reverse">
+                    <img class="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="profile picture" />
+                    <div class="flex items-center justify-center divide-x-2 rtl:divide-x-reverse divide-gray-300 dark:divide-gray-700">
+                      <cite class="pe-3 font-medium text-gray-900 dark:text-white">{reviews.name}</cite>
+                      <cite class="ps-3 text-sm text-gray-500 dark:text-gray-400">Google Reviews</cite>
+                    </div>
+                  </figcaption>
+
+                  <div className="rating rating-lg rating-half justify-center">
+                    <input type="radio" name="rating-10" className="rating-hidden" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-1" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-2" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-1" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-2" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-1" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-2" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-1" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-2" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-1" />
+                    <input type="radio" name="rating-10" className="bg-orange-400 mask mask-star-2 mask-half-2" checked />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="absolute hidden sm:flex justify-between transform -translate-y-1/2 left-5 right-5 md:left-20 md:right-20 lg:left-40 lg:right-40 xl:left-60 xl:right-60 2xl:left-80 2xl:right-80 top-1/2">
+              <a href={`#slide${i - 1}`} className="btn btn-circle">❮</a>
+              <a href={`#slide${i + 1}`} className="btn btn-circle">❯</a>
+            </div>
           </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">❮</a>
-            <a href="#slide3" className="btn btn-circle">❯</a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">❮</a>
-            <a href="#slide4" className="btn btn-circle">❯</a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">❮</a>
-            <a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
-        </div>
+        ))}
+
+
       </div>
 
 
 
-    </div>
+    </div >
   )
 }
 
