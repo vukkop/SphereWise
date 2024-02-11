f88d5503bbf32e5cb8b468bbf1fa00063a0ce9f1
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-02-10 17:19:18 -0800

    Updated Hero

--- client/src/components/home-components/Hero.jsx
+++ client/src/components/home-components/Hero.jsx
@@ -6,15 +6,18 @@ const Hero = () => {
     <div className='container mx-auto'>
       <div className="hero min-h-screen">
         <div className="hero-content flex-col-reverse lg:flex-row-reverse">
-          <div className='relative h-[70vh] w-full rounded-br-2xl rounded-bl-2xl lg:rounded-tr-2xl lg:rounded-br-2xl lg:rounded-bl-none overflow-hidden border-none -mt-7'>
+          <div className='relative h-[70vh] w-full rounded-br-2xl rounded-bl-2xl lg:rounded-tr-2xl lg:rounded-br-2xl lg:rounded-bl-none overflow-hidden border-none -mt-4'>
             <img src={Idea} className="absolute insert-0 h-full w-full object-cover filter grayscale contrast-200" />
             <div className='absolute insert-0 h-full w-full bg-gradient-to-b lg:bg-gradient-to-r from-base-100  to-transparent'></div>
           </div>
-          <div className='w-[70%]'>
-            <h2 className="text-left text-4xl mt-5">Tailored Solutions for Every Business Size</h2>
-            <p className="text-left mt-5">At SphereWise Tech, we champion the growth of businesses, regardless of size. Our bespoke website and software solutions aren't confined by scale; they're crafted to elevate small businesses to new heights. We believe that no endeavor is too modest. From startups to local enterprises, our commitment remains steadfast in delivering intuitive websites and custom software that catalyze growth. Our focus? Empowering every business, ensuring they harness the power of technology to expand their horizons and redefine success. Let's embark on this journey together—transforming small ventures into impactful enterprises, one innovative solution at a time.
-            </p>
-            <div className="justify-center mt-3 mb-3">
+          <div className='w-[100%]'>
+            <h1 className="text-left text-5xl lg:text-6xl font-bold leading-tight">Empower your business with SphereWise Tech</h1>
+            <div className='flex items-top gap-6 mt-8 lg:mt-14'>
+              <i class="fa-solid fa-right-long text-3xl mt-[5px]"></i>
+              <h2 className="text-left text-3xl ">  Our tailored website and software solutions transcend size limitations, elevating startups and local enterprises to new heights.
+              </h2>
+            </div>
+            <div className="justify-center mt-10">
               <button className="btn btn-outline btn-info">Find Out More</button>
             </div>
           </div>
