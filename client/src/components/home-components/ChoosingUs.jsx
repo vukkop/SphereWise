716c57e09b4bfd6b9319652f725b6d134b2f0d6e
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-02-16 12:32:34 -0800

    Choosin us

--- client/src/components/home-components/ChoosingUs.jsx
+++ client/src/components/home-components/ChoosingUs.jsx
@@ -10,42 +10,42 @@ const ChoosingUs = ({ isLight }) => {
             <span className='font-bold text-xl'>1</span>
           </div>
           <h4 className='text-xl font-semibold mb-1'>Certified experts</h4>
-          <p>Certified business and tech development experts who ensure a business-focused approach.</p>
+          <p>Experts in business and tech development with certifications, dedicated to ensuring a business-centric approach.</p>
         </div>
         <div>
           <div className={`rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 ${isLight ? 'via-sky-500' : 'via-blue-600'} to-transparent`}>
             <span className='font-bold text-xl'>2</span>
           </div>
           <h4 className='text-xl font-semibold mb-1'>Quick implementation</h4>
-          <p>Velocity doubled by quality is what our Agile approach boils down to.</p>
+          <p>Our Agile approach essentially means doubling velocity through the integration of quality.</p>
         </div>
         <div>
           <div className={`rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 ${isLight ? 'via-sky-500' : 'via-blue-600'} to-transparent`}>
             <span className='font-bold text-xl'>3</span>
           </div>
           <h4 className='text-xl font-semibold mb-1'>Technology Agnostic</h4>
-          <p>Providing a tech stack wide and expansive enough to erase any limits of flexibility during each and every project.</p>
+          <p>Delivering an extensive tech stack that eliminates flexibility constraints in every project.</p>
         </div>
         <div>
           <div className={`rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 ${isLight ? 'via-sky-500' : 'via-blue-600'} to-transparent`}>
             <span className='font-bold text-xl'>4</span>
           </div>
           <h4 className='text-xl font-semibold mb-1'>Flexibility</h4>
-          <p>We can integrate your product with any required platform so that it moves your business forward.</p>
+          <p>Seamless integration of your product with any necessary platform, propelling your business forward.</p>
         </div>
         <div>
           <div className={`rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 ${isLight ? 'via-sky-500' : 'via-blue-600'} to-transparent`}>
             <span className='font-bold text-xl'>5</span>
           </div>
           <h4 className='text-xl font-semibold mb-1'>Compliance</h4>
-          <p>Regardless of the industry, we build software that does not react to, but prevents cyber security and legal issues.</p>
+          <p>Industry-agnostic, our software is designed not to react but to proactively prevent cybersecurity and legal issues.</p>
         </div>
         <div>
           <div className={`rounded-xl mb-1 px-4 py-2 w-fit bg-gradient-to-br from-sky-400 ${isLight ? 'via-sky-500' : 'via-blue-600'} to-transparent`}>
             <span className='font-bold text-xl'>6</span>
           </div>
           <h4 className='text-xl font-semibold mb-1'>Adaptability</h4>
-          <p>Avenga builds products with codes, adapting to the client's requirements, not vice versa.</p>
+          <p>At SphereWise Tech, we develop products with adaptable code that conforms to the client's requirements, not the other way around.</p>
         </div>
       </div>
     </div>
