c7b6d8226c9bc1293a280d079a6586b1b224f1e8
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2024-03-03 17:04:26 -0800

    Added Data

--- client/src/components/services-components/ServicesData.js
+++ client/src/components/services-components/ServicesData.js
@@ -2,7 +2,9 @@ export const services = [
   {
     heading: "Custom Website Development",
     title: "Elevate Your Business with Tailored Website Development Services",
-    description: [
+    description:
+      "Welcome to our Custom Website Development services page at SphereWise Tech. Discover the power of tailored and responsive websites designed to reflect your brand and achieve your business objectives.",
+    expertisePoints: [
       {
         point: "Visually Captivating Design:",
         details:
@@ -14,7 +16,7 @@ export const services = [
           "We ensure your website functions flawlessly across all devices, providing an exceptional user experience.",
       },
     ],
-    expertisePoints: [
+    chooseUsPoints: [
       {
         point: "Unique Brand Identity:",
         details:
@@ -26,85 +28,102 @@ export const services = [
           "Beyond aesthetics, our focus is on delivering tangible business results through your online platform.",
       },
     ],
-
     slug: "websites",
   },
   {
-    heading: "",
-    title: "",
-    description: [
-      {
-        point: "",
-        details: "",
-      },
-      {
-        point: "",
-        details: "",
-      },
-    ],
+    heading: "Custom App Development",
+    title:
+      "Transform Your Ideas into Innovative Apps with Our Custom App Development Solutions",
+    description:
+      "Unlock the potential of innovation through our Custom App Development services at SphereWise Tech. Our team specializes in crafting user-friendly applications that enhance productivity and align with your business goals.",
     expertisePoints: [
       {
-        point: "",
-        details: "",
+        point: "Intuitive User-Centric Design:",
+        details:
+          "We prioritize creating apps with user-friendly interfaces to ensure a positive user experience.",
       },
       {
-        point: "",
-        details: "",
+        point: "Optimized Functionality and Performance:",
+        details:
+          "Our developers focus on building apps that not only look great but also deliver exceptional performance.",
       },
     ],
-
+    chooseUsPoints: [
+      {
+        point: "Strategic Alignment:",
+        details:
+          "We align our app development with your business strategy, providing solutions that precisely meet your needs.",
+      },
+      {
+        point: "Continuous Technological Evolution:",
+        details:
+          "Stay ahead with cutting-edge app solutions that evolve alongside your business.",
+      },
+    ],
     slug: "applications",
   },
   {
-    heading: "",
-    title: "",
-    description: [
-      {
-        point: "",
-        details: "",
-      },
-      {
-        point: "",
-        details: "",
-      },
-    ],
+    heading: "Bespoke Software Solutions",
+    title:
+      "Precision and Innovation Unleashed - Explore Our Bespoke Software Solutions",
+    description:
+      "Welcome to the realm of precision and innovation at SphereWise Tech's Bespoke Software Solutions page. Our custom software development services are meticulously crafted to address your unique business needs.",
     expertisePoints: [
       {
-        point: "",
-        details: "",
+        point: "Comprehensive Problem Solving:",
+        details:
+          "We analyze your business requirements to develop software solutions that comprehensively address your challenges.",
       },
       {
-        point: "",
-        details: "",
+        point: "Built for Scalability:",
+        details:
+          "Our solutions are crafted with scalability in mind, ensuring they grow seamlessly with your business.",
       },
     ],
-
+    chooseUsPoints: [
+      {
+        point: "Collaborative Partnership:",
+        details:
+          "We work closely with you to understand your goals, ensuring our software solutions align with your vision.",
+      },
+      {
+        point: "Sustainable Success:",
+        details:
+          "Our focus is on laying the groundwork for your long-term success through innovative and adaptive software solutions.",
+      },
+    ],
     slug: "software",
   },
   {
-    heading: "",
-    title: "",
-    description: [
-      {
-        point: "",
-        details: "",
-      },
-      {
-        point: "",
-        details: "",
-      },
-    ],
+    heading: "E-commerce Solutions",
+    title:
+      "Amplify Your Online Presence with Comprehensive E-commerce Solutions",
+    description:
+      "Embark on a journey into the digital marketplace with SphereWise Tech's comprehensive E-commerce Solutions. From initial setup to ongoing optimization, we offer a complete suite of services to boost your online presence.",
     expertisePoints: [
       {
-        point: "",
-        details: "",
+        point: "Efficient E-commerce Setup:",
+        details:
+          "We assist in setting up your online store, ensuring a seamless shopping experience for your customers.",
       },
       {
-        point: "",
-        details: "",
+        point: "Continuous Optimization:",
+        details:
+          "Our strategies focus on optimizing your e-commerce platform for increased sales and enhanced customer satisfaction.",
       },
     ],
-
+    chooseUsPoints: [
+      {
+        point: "Tailored E-commerce Strategies:",
+        details:
+          "We understand the nuances of e-commerce and tailor our strategies to fit your unique business model.",
+      },
+      {
+        point: "Results-Driven Focus:",
+        details:
+          "From increased conversions to improved customer retention, our goal is to drive measurable results.",
+      },
+    ],
     slug: "e-commerce",
   },
 ];
